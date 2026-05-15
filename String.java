991aaa6a7e25b9c8cc3efd757e846c558431c516
JagrutiSonani <sonanijagruti22@gmail.com>
2026-05-15 19:16:04 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,11 @@
+class String {
+    public static void main(String[] args) {
+
+        String name = "Jagruti";
+
+        System.out.println(name.length());
+		System.out.println(name.toUpperCase());
+		System.out.println(name.charAt(2));
+		
+    }
+}
